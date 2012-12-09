1b540d0ee485cb2631a1bfcc36022e15d63bcd54
demiantill <demiantill@gmail.com>
2012-12-09 16:42:59 +0000

    fixing SlidingPiece to fit with the new implementation of the various piece classes

--- chess/src/main/java/chess/piece/SlidingPiece.java
+++ chess/src/main/java/chess/piece/SlidingPiece.java
@@ -1,34 +1,15 @@
+
 package chess.piece;
 
 import chess.util.Colour;
 /**
  * 
- * @author Craig Martin
- * A sliding piece is a piece which can move multiple squares in a row, and it is not a pawn
+ * @author Craig Martin, Demian Till
+ * A sliding piece is a piece that can take multiple steps in it's allowed directions in a single turn
  */
 public class SlidingPiece extends NonPawn {
 
-	//Moves for the bishop: leftup, rightup, rightdown, leftdown   
-		private final byte[] BishDir = new byte[]{(byte)15,(byte)17,(byte)-15,(byte)-17};
-		//Moves for the rook: Up, Down, Left, Right
-		private final byte[] RookDir = new byte[]{(byte)16,(byte)-16,(byte)-1,(byte)1};
-		//Moves for the Queen:leftup, rightup, rightdown, leftdown,Up, Down, Left, Right, 
-		private final byte[] QueenDir = new byte[]{(byte)15,(byte)17,(byte)-15,(byte)-17,(byte)16,(byte)-16,(byte)-1,(byte)1};
-		
-	public SlidingPiece(Colour c, Type t, byte coordinate) throws InvalidPiece {
-		super(c, t, coordinate);
-		if (!(t == Type.QUEEN || t == Type.ROOK || t == Type.BISHOP)){
-			throw new InvalidPiece("Sliding piece must be of type Bishop,Queen or Rook");
-		}
+	public SlidingPiece(Colour c, Type t, byte position) {
+		super(c, t, position);
 	}
-
-	public byte[] getDirections() {
-		Type type = this.getType();
-		switch(type){
-		case BISHOP:	return BishDir;
-		case ROOK:		return RookDir;
-		default:		return QueenDir; 
-		}
-	}
-
 }
