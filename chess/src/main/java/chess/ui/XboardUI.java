b08e8d95cc4d46491e56023a6759c29bb4223a32
rafaelborsoe <rafaelborsoe@googlemail.com>
2013-03-18 21:36:57 +0000

    file

--- chess/src/main/java/chess/ui/XboardUI.java
+++ chess/src/main/java/chess/ui/XboardUI.java
@@ -1,6 +1,9 @@
 package chess.ui;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.regex.Matcher;
@@ -13,8 +16,17 @@ public class XboardUI implements UI {
 	private static Pattern coordinateMovePattern = Pattern.compile(".*([a-h][1-8][\\W]*[a-h][1-8][qrnbQRNB]?)");
 	private final BufferedReader stdin;
 
+	private BufferedWriter w;
+
 	public XboardUI() {
 		stdin = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			File f = new File("move.txt");
+			f.createNewFile();
+			w = new BufferedWriter(new FileWriter(f));
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 		write("feature usermove=1");
 		write("feature option=NAME -button");
 		write("feature done=1");
@@ -27,6 +39,10 @@ public class XboardUI implements UI {
 		do {
 			try {
 				input = stdin.readLine();
+				if (w != null) {
+					w.write(input);
+					w.flush();
+				}
 				if (coordinateMovePattern.matcher(input).matches()) {
 					Matcher matcher = coordinateMovePattern.matcher(input);
 					if (matcher.find())
